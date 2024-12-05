977b10700bd266e5991c4fffbe71cce060a8a7a3
Gnanakoushik <jpranathi04@gmail.com>
2024-12-05 22:47:59 +0530

    Add files via upload

---
+++ Example.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Example{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        for(int i=2;i<=t;i++)
+        {
+            int c=sc.nextInt();
+            c=(i*2);
+            {
+                System.out.println(c);
+            }
+            
+            
+
+            
+            
+        }
+    }
+}
