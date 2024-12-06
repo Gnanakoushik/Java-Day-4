4915a3ec6982de8881f5d6e456f081032a113a4c
Gnanakoushik <jpranathi04@gmail.com>
2024-12-06 06:42:25 +0530

    Add files via upload

---
+++ Exampleforwhile.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Exampleforwhile{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.println(n++);
+        int a=sc.nextInt();
+        System.out.println(++a);
+        int b=sc.nextInt();
+        System.out.println(b--);
+        int c=sc.nextInt();
+        System.out.println(--c);
+    }
+}
